e459bfe7edf08d69bfd5c4e00467100e5434920f
shachis <shachi13.ss@gmail.com>
2026-01-19 13:40:17 -0500

    Create .eleventy.js

---
+++ shachi-singh-site/.eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
